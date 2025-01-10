4b69807f9e2b69d50fb122ccba76b87bd055fee0
thocorLHM <187379909+thocorLHM@users.noreply.github.com>
2025-01-10 11:37:35 +0100

    Initiating Frontend Service

---
+++ quad/src/test/java/com/example/quad/QuadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
