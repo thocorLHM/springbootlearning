3be5df6e642168726db17f754d3dde6ad1ebd01e
thocorLHM <187379909+thocorLHM@users.noreply.github.com>
2025-01-21 10:56:01 +0100

    QuadService wurde um die Methode quadNumberandSave(Integer inputNumber) erweitert. Führt die Kernlogik für die Berechnung und Speicherung durch. Erstellten/Berechneten Daten werden über das Repository in der Datenbank gespeichert.

--- quadrierungsbackend/src/main/java/com/example/quad/services/QuadService.java
+++ quadrierungsbackend/src/main/java/com/example/quad/services/QuadService.java
@@ -1,11 +1,30 @@
 package com.example.quad.services;
 
+import com.example.quad.entities.Calculation;
+import com.example.quad.repository.CalculationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class QuadService {
 
-    public int quadNumber(int number){
-        return number*number;
+    @Autowired
+    private CalculationRepository calculationRepository;
+
+    public int quadNumber(int inputNumber) {
+
+        return inputNumber*inputNumber;
+    }
+
+    public Calculation quadNumberandSave(Integer inputNumber){
+
+        Calculation calculation = new Calculation();
+        calculation.setInputNumber(inputNumber);
+        calculation.setSquaredNumber(inputNumber*inputNumber);
+        calculation.setCalculatedAt(LocalDateTime.now());
+        calculationRepository.save(calculation);
+        return calculation;
     }
 }
