764c65fc0e21474d2cf57f51becf6fd21cfc74a2
thocorLHM <187379909+thocorLHM@users.noreply.github.com>
2025-01-28 16:15:01 +0100

    Anpassung von SendingService an den neuen Aufbau des Servicelayer,Entfernen von ungenutzten import-statements.

--- quadrierung/apigateway/src/main/java/de/muenchen/tc/group/servicelayer/SendingService.java
+++ quadrierung/apigateway/src/main/java/de/muenchen/tc/group/servicelayer/SendingService.java
@@ -1,39 +1,17 @@
 package de.muenchen.tc.group.servicelayer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
 
 @Service
-public class SendingService {
+public final class SendingService {
+
+    @Autowired
+    HttpRequestService httpRequestService;
 
     private static final String SERVICE_URL = "http://localhost:4712/quad";
 
-    public String sendNumber(final int number) {
-        try {
-            // JSON-String mit der Nummer erstellen
-            String jsonBody = String.valueOf(number);
-
-            // HttpClient erstellen
-            HttpClient client = HttpClient.newHttpClient();
-
-            // HttpRequest konfigurieren
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(SERVICE_URL))
-                    .header("Content-Type", "application/json")
-                    .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
-                    .build();
-
-            // Anfrage senden und Antwort verarbeiten
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-            // Antwort zurückgeben
-            return "Antwort von Service: " + response.body();
-        } catch (Exception e) {
-            return "Fehler beim Senden der Nummer: " + e.getMessage();
-        }
+    public String sendNumbertoBackend(final int number) {
+        return httpRequestService.sendPostRequest(SERVICE_URL, number);
     }
 }
