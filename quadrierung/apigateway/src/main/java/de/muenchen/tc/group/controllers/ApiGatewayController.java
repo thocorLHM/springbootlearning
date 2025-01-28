9ea6d8eae0648a0447c1de6cb6600e509510cf29
thocorLHM <187379909+thocorLHM@users.noreply.github.com>
2025-01-28 15:58:26 +0100

    @GetMapping Methoden in einen Controller zusammengeführt für bessere Lesbarkeit.

--- quadrierung/apigateway/src/main/java/de/muenchen/tc/group/controllers/ApiGatewayController.java
+++ quadrierung/apigateway/src/main/java/de/muenchen/tc/group/controllers/ApiGatewayController.java
@@ -1,6 +1,7 @@
 package de.muenchen.tc.group.controllers;
 
 
+import de.muenchen.tc.group.servicelayer.DatabankSendingService;
 import de.muenchen.tc.group.servicelayer.SendingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -9,11 +10,21 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class ApiGatewayController {
 
+    //Für die einfache Quadrierung
     @Autowired
     SendingService sendingService;
 
+    //Für die Quadrierung und für das Schreiben des Ergebnisses in die Datenbank
+    @Autowired
+    DatabankSendingService databankSendingService;
+
     @GetMapping(path="/cal")
     public String setNumber(@RequestParam(required = false) Integer number) {
-        return sendingService.sendNumber(number);
+        return sendingService.sendNumbertoBackend(number);
+    }
+
+    @GetMapping(path = "/sending")
+    public String setNumberforDatabank(@RequestParam(required = false) Integer number) {
+        return databankSendingService.sendNumbertoDatabank(number);
     }
 }
