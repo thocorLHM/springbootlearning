3a0a76de7ebb093151b8a5ede87b475736743e02
thocorLHM <187379909+thocorLHM@users.noreply.github.com>
2025-01-28 16:05:18 +0100

    Ordentlichkeitsarbeit

--- quadrierungsbackend/src/main/java/com/example/quad/QuadApplication.java
+++ quadrierungsbackend/src/main/java/com/example/quad/QuadApplication.java
@@ -1,9 +1,7 @@
 package com.example.quad;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 @SpringBootApplication
 public class QuadApplication {
